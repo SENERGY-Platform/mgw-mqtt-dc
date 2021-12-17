9053357c349afbfd23269182a40923a61ed3be82
Ingo <roessner@infai.org>
2021-12-17 14:10:17 +0100

    implement main.go

--- main.go
+++ main.go
@@ -18,10 +18,41 @@ package main
 
 import (
 	"context"
+	"flag"
 	"github.com/SENERGY-Platform/mgw-mqtt-dc/pkg/configuration"
 	"github.com/SENERGY-Platform/mgw-mqtt-dc/pkg/connector"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	connector.New(context.Background(), configuration.Config{})
+	configLocation := flag.String("config", "config.json", "configuration file")
+	flag.Parse()
+
+	config, err := configuration.Load(*configLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	_, err = connector.New(ctx, config)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	go func() {
+		shutdown := make(chan os.Signal, 1)
+		signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+		sig := <-shutdown
+		log.Println("received shutdown signal", sig)
+		cancel()
+	}()
+
+	<-ctx.Done()                //waiting for context end; may happen by shutdown signal
+	time.Sleep(1 * time.Second) //give go routines time for cleanup
 }
